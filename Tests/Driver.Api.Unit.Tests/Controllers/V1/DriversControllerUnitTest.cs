4a2c8da606eb4f8def3c706100f496f88fb50889
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2023-11-20 13:30:19 +0400

    cover get and get all api

--- Tests/Driver.Api.Unit.Tests/Controllers/V1/DriversControllerUnitTest.cs
+++ Tests/Driver.Api.Unit.Tests/Controllers/V1/DriversControllerUnitTest.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using AutoFixture;
 using Driver.Api.Controllers.V1;
 using Driver.Application.Services.Driver;
 using Driver.Common.DTO.Driver;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace Driver.Api.Unit.Tests.Controllers.V1
@@ -18,18 +20,37 @@ namespace Driver.Api.Unit.Tests.Controllers.V1
         }
 
         [Fact]
-        public async Task GetAllAsync_Return_Ok()
+        public async Task GetAsync_Return_Ok()
         {
             //Arrange
-            var result = Fixture.Build<List<DriverDto>>().Create();
-            _driverServiceMock.Setup(x => x.GetAllAsync())
+            var id = Guid.NewGuid();
+            var result = Fixture.Build<DriverDto>().With(c => c.Id , id).Create();
+            _driverServiceMock.Setup(x => x.GetAsync(id))
                 .Returns(Task.FromResult(result));
 
             //Act
-            var response = await _controller.GetAllAsync();
+            var response = (OkObjectResult) await _controller.GetAsync(id);
 
             //Assert
-            Assert.True(true);
+            Assert.Equal(response.StatusCode , 200);
+        }
+
+
+        [Fact]
+        public async Task GetAllAsync_Return_Ok()
+        {
+            //Arrange
+            var result = Fixture.Build<DriverDto>().CreateMany(3);
+
+            _driverServiceMock.Setup(x => x.GetAllAsync())
+                .ReturnsAsync(result.ToList);
+
+            //Act
+            var response = (OkObjectResult)await _controller.GetAllAsync();
+            var list = (List<DriverDto>)response.Value;
+            //Assert
+            Assert.Equal(response.StatusCode, 200);
+            Assert.Equal(list.Count, 3);
         }
 
 
